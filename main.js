e4e0541df955d9d91470cb5fb8138743ad1f872e
Lucas de Oliveira Dantas <LucasdeOliveiradantas@protonmail.com>
2021-12-30 17:57:01 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const btn1 = document.querySelector('.faq1')
+const btntext1 = document.querySelector('.faq1')
+const desc1 = document.querySelector('.faq1-desc')
+
+btn1.addEventListener('click', function () {
+  desc1.classList.toggle('show')
+  btntext1.classList.toggle('active')
+})
+/* ###### */
+const btn2 = document.querySelector('.faq2')
+const btntext2 = document.querySelector('.faq2')
+const desc2 = document.querySelector('.faq2-desc')
+
+btn2.addEventListener('click', function () {
+  desc2.classList.toggle('show')
+  btntext2.classList.toggle('active')
+})
+/* ###### */
+const btn3 = document.querySelector('.faq3')
+const btntext3 = document.querySelector('.faq3')
+const desc3 = document.querySelector('.faq3-desc')
+
+btn3.addEventListener('click', function () {
+  desc3.classList.toggle('show')
+  btntext3.classList.toggle('active')
+})
+/* ###### */
+const btn4 = document.querySelector('.faq4')
+const btntext4 = document.querySelector('.faq4')
+const desc4 = document.querySelector('.faq4-desc')
+
+btn4.addEventListener('click', function () {
+  desc4.classList.toggle('show')
+  btntext4.classList.toggle('active')
+})
+/* ###### */
+const btn5 = document.querySelector('.faq5')
+const btntext5 = document.querySelector('.faq5')
+const desc5 = document.querySelector('.faq5-desc')
+
+btn5.addEventListener('click', function () {
+  desc5.classList.toggle('show')
+  btntext5.classList.toggle('active')
+})
